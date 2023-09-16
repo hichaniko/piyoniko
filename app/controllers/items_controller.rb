4ef3b91326bf7bafa54dbada30b69927ce686e00
hichaniko <hichaniko1127@gmail.com>
2023-09-16 21:24:18 +0900

    保存機能、アクティブハッシュ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,18 @@ class ItemsController < ApplicationController
   def index
     # @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:text, :image).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,4 @@
+class Item < ApplicationRecord
+  validates :text, presence: true
+  has_one_attached :image
+end
